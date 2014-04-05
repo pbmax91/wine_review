13a9a42cd4ffeb61b84cd1ca2629f7c98f08acad
pbmax91 <pbmax91@gmail.com>
2014-04-04 21:48:35 -0400

    lab9 homework

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -6,4 +6,19 @@ class WinesController < ApplicationController
 	def show
 		@wine = Wine.find(params[:id])
 	end
+	def new
+	  @wine = Wine.new
+	end
+
+	def create
+	  @wine = Wine.new(wine_params)
+	  @wine.save
+	  redirect_to @wine
+	end
+
+	private
+
+	def wine_params
+	  params.require(:wine).permit(:name, :year, :winery, :country, :varietal)
+	end
 end
