f7129fabae8c282a35c9c1b3e0e77a81baa050af
pbmax91 <pbmax91@gmail.com>
2014-04-04 22:43:09 -0400

    final push

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,10 +1,11 @@
 class WinesController < ApplicationController
+	before_action :set_wine, only: [ :show, :edit, :update, :destroy ]
+
 	def index
 		@available_at = Time.now
 		@wines = Wine.all
 	end
 	def show
-		@wine = Wine.find(params[:id])
 	end
 	def new
 	  @wine = Wine.new
@@ -15,15 +16,12 @@ class WinesController < ApplicationController
 	  redirect_to @wine
 	end
 	def edit
-	  @wine = Wine.find(params[:id])
 	end
 	def update
-	  @wine = Wine.find(params[:id])
 	  @wine.update(wine_params)
 	  redirect_to @wine
 	end
 	def destroy
-	  @wine = Wine.find(params[:id])
 	  @wine.destroy
 	  redirect_to wines_url
 	end
@@ -32,5 +30,8 @@ class WinesController < ApplicationController
 
 	def wine_params
 	  params.require(:wine).permit(:name, :year, :winery, :country, :varietal)
+	end
+	def set_wine
+	  @wine = Wine.find(params[:id])
 	end
 end
