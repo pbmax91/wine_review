a9bf09f5ed09ea8633339fee993c6cfbc1234f78
pbmax91 <pbmax91@gmail.com>
2014-04-04 22:32:10 -0400

    upload2

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -9,11 +9,23 @@ class WinesController < ApplicationController
 	def new
 	  @wine = Wine.new
 	end
-
 	def create
 	  @wine = Wine.new(wine_params)
 	  @wine.save
 	  redirect_to @wine
+	end
+	def edit
+	  @wine = Wine.find(params[:id])
+	end
+	def update
+	  @wine = Wine.find(params[:id])
+	  @wine.update(wine_params)
+	  redirect_to @wine
+	end
+	def destroy
+	  @wine = Wine.find(params[:id])
+	  @wine.destroy
+	  redirect_to wines_url
 	end
 
 	private
