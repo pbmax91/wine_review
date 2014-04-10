19e8fabbb6400d27dc2bddd2c810cb6b35e9d856
pbmax91 <pbmax91@gmail.com>
2014-04-09 22:05:35 -0400

    2nd push lab 10

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 WineReview::Application.routes.draw do
   resources :wines
+  root 'wines#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
