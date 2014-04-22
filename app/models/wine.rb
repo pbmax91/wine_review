e24508171a797d6cecc391cfad6980b9733984eb
pbmax91 <pbmax91@gmail.com>
2014-04-21 22:57:18 -0400

    initial commit lab 11

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -4,4 +4,5 @@ validates :year,
    numericality: { only_integer: true, greater_than_or_equal_to: 0 },
    unless: "year.blank?"
 validates :varietal, :inclusion=>{:in=>%w(Cabernet Merlot Chianti Bordeux)}, :allow_nil=>true, :allow_blank=>true
+has_many :logentries
 end
