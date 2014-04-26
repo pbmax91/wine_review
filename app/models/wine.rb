fb9db4594e6341ca3aad3e9c53441605741e2604
pbmax91 <pbmax91@gmail.com>
2014-04-25 23:27:12 -0400

    lab11 push

--- app/models/wine.rb
+++ app/models/wine.rb
@@ -4,7 +4,7 @@ class Wine < ActiveRecord::Base
 	   numericality: { only_integer: true, greater_than_or_equal_to: 0 },
 	   unless: "year.blank?"
 	validates :varietal, :inclusion=>{:in=>%w(Cabernet Merlot Chianti Bordeux)}, :allow_nil=>true, :allow_blank=>true
-	has_many :logentries
+	has_many :logentries, dependent: :destroy
 	def average_stars
 	  logentries.average(:rating)
 	end
